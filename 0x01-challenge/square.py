ae058641d99f7bc5bb138cdd1b700a0c93f9b7ed
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-15 19:36:36 +0000

    solved style

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
+
 class Square():
 
     width = 0
     height = 0
-
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
@@ -19,6 +19,7 @@ class Square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
