853fb4971bf931a765c9b02bd2805254fb9de96d
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-15 18:51:15 +0000

    updated

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""square"""
-
 
 class square():
-    """Square class"""
+
     width = 0
     height = 0
+
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
@@ -20,7 +19,6 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
-
 
 if __name__ == "__main__":
 
